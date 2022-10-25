6606ae537ada50da4b459d9365b8990d78609238
Danny Julca C <djulcac@uni.pe>
2022-10-24 23:08:35 -0500

    mod

---
+++ codeforces/584b.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define _int int
+#define f4(i,a,b,step) for(_int i=a,_=a<b,_2=abs(step);(_&&i<b)||(!_&&i>b);_?i+=_2:i-=_2)
+#define F(i,a,b) for(_int i=a,_=a<b;(_&&i<b)||(!_&&i>b);_?i++:i--)
+#define f(i,n) for(_int i=0;i<n;i++)
+#define MOD 1000000007
+ll solve(){
+	ll n;cin>>n;
+	ll ans=0,a=1,m=1;
+	f(i,n)a=(a*27)%MOD,m=(m*7)%MOD;
+	ans=((a-m)%MOD+MOD)%MOD;
+	return ans;
+}
+
+int main(){
+	ll T = 1;
+	// cin>>T;
+	// while(T--) solve();
+	while(T--) cout<<solve()<<endl;
+	return 0;
+}
+//3
